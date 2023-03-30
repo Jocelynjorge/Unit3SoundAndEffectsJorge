fd347524206059d29b5fa610d079db1acdb1ab4f
Jorge <719727@student.dusd.net>
2023-03-30 11:37:27 -0700

    Finished 3.3

--- Unit3SoundAndEffectsJorgeP4/Assets/Scripts/PlayerController.cs
+++ Unit3SoundAndEffectsJorgeP4/Assets/Scripts/PlayerController.cs
@@ -9,7 +9,9 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpForce;
     public float gravityModifier;
- 
+    private Animator playerAnim;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
 
 
 
@@ -19,38 +21,57 @@ public class PlayerController : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+        playerAnim = GetComponent<Animator>();
     }
 
 
 
 
     public bool isOnGround = true;
+    public ParticleSystem dirtParticle; 
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
+            dirtParticle.Stop();
+
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+        }
             
         
-        }
+        
     }
 
     public bool gameOver = false;
+    public ParticleSystem explosionParticle;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
+            dirtParticle.Play();
             isOnGround = true;
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
+            explosionParticle.Play();
+            dirtParticle.Stop();
+        
+
+
             gameOver = true;
             Debug.Log("Game Over");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
+     
+
+   
+
 
 }
 
